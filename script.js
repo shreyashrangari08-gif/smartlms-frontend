714a510abd0f59af8d6e31b6a3abbe06c887396c
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-05-27 11:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const API_URL = "https://lms-backrnd.onrender.com";
+
+async function fetchData() {
+    try {
+        const response = await fetch(`${API_URL}/`);
+        const data = await response.text();
+        document.getElementById('data-container').innerText = data;
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+fetchData();
